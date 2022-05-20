ef7fedd4a561bcd9926182e189c317590099ddfc
Andrew <46112160+Fybex@users.noreply.github.com>
2022-05-20 12:25:42 +0300

    Add min/max

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def index() -> str:
 def request_handler(func: callable) -> callable:
     def wrapper():
         data = request.get_json()
-        total = func(data)
+        total = func(data.values())
         return jsonify(
             total=total,
         )
@@ -24,12 +24,22 @@ def request_handler(func: callable) -> callable:
 @app.route('/add', methods=['POST'], endpoint='add')
 @request_handler
 def add(data: list[int]) -> int:
-    return reduce(lambda x, y: x + y, data)
+    return sum(data)
 
 
 @app.route('/multiply', methods=['POST'], endpoint='multiply')
 @request_handler
 def multiply(data: list[int]) -> int:
     return reduce(lambda x, y: x * y, data)
+
+
+@app.route('/minmax', methods=['POST'], endpoint='minmax')
+@request_handler
+def minmax(data: list[int]) -> dict[str, int]:
+    return {
+        'min': min(data),
+        'max': max(data),
+    }
+
 
 
